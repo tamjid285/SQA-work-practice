18c5e0a3dc09d976fc6117d7ed4ccc917a07f6ca
Baishakh Islam <baishakh258@gmail.com>
2024-02-05 17:48:15 +0600

    sqa

---
+++ src/Java1/Identifie.java
@@ -0,0 +1,14 @@
+package Java1;
+
+public class Identifie {
+    int number = 10;
+
+    public void number(){
+        System.out.println("The Number"+ number);
+    }
+
+    public static void main(String[] args) {
+        Identifie sum=new Identifie();
+
+    }
+}
---
+++ src/Java1/moreClass.java
@@ -0,0 +1,12 @@
+package Java1;
+
+public class moreClass {
+    static int number = 10;
+
+    public static void main(String[] args) {
+        System.out.println(number);
+
+    }
+
+}
+
